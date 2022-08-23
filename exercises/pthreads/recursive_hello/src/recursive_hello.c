e19264346bab94bca69539da07659bb01017de3e
Daniel Alberto Perez Morera <c15906@ECCI104L408200.ecci.ucr.ac.cr>
2022-08-23 11:43:57 -0600

    recursive_hello: contiene desing del codigo

---
+++ exercises/pthreads/recursive_hello/src/recursive_hello.c
@@ -0,0 +1,75 @@
+/*
+*Copyright 2022 Daniel Perez Morera <daniel.perezmorera@ucr.ac.cr> CC-BY 4.0
+*Imprime un hello
+*/
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * @brief  Genera un mensaje en la salida standar
+ * 
+ * @param data NULL
+ * @return void* NULL
+ */
+void* greet(void* data);
+
+//  procedure main():
+int main(void) {
+  const size_t greet_number = 2;
+  //  create_thread(great())
+  pthread_t thread;
+  //  CREA HILO SECUNDARIO
+  int error = pthread_create(&thread , /*attr*/ NULL , greet , (void*)greet_number);
+  if (error == EXIT_SUCCESS) {
+    //  print "Hello from main thread"
+    printf("Hello from main thread\n");
+    pthread_join(thread, /*value_ptr*/ NULL);  //  ENVIA A DORMIR HILO PRINCIPAL
+  } else {
+    fprintf(stderr , "Error: could not create secundary thread\n");
+  }
+  return error;
+}  //  end procedure
+
+//  procedure great():
+
+voidgit * greet(void* data) {
+  const size_t greet_number = (size_t) data;
+  int error = 0;
+  pthread_t thread;
+  if(greet_number == 0) {
+    printf("Bye\nNumber: %zu\n", greet_number);
+  }else if(greet_number > 0) {
+    printf("Hello from secundary thread\nNumber: %zu\n", greet_number);
+    error = pthread_create(&thread , /*attr*/ NULL , greet , (void*)greet_number - 1);
+    if (error == EXIT_SUCCESS) {
+      pthread_join(thread, /*value_ptr*/ NULL); 
+    }else{
+      fprintf(stderr , "Error: could not create secundary thread\n");
+    }
+  }else{
+    fprintf(stderr , "Error: Out of range number\n");
+  }
+  return NULL;
+}  //  end procedure
+
+
+/*
+Salida esperada:
+Hello from main thread
+Hello from secundary thread
+Number: 2
+Hello from secundary thread
+Number: 1
+Bye
+NUmber: 0
+
+Salida generada:
+Hello from main thread
+Hello from secundary thread
+Number: 2
+Hello from secundary thread
+Number: 1
+Bye
+Number: 0
+*/

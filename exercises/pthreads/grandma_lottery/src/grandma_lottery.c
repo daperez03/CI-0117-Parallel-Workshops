3a3929f4d1df9e4b8bb920c2be3f40761f946395
Daniel Alberto Perez Morera <c15906@ECCI104L408200.ecci.ucr.ac.cr>
2022-08-26 11:08:02 -0600

    Solucion de ejercicio grandma_loterry

--- exercises/pthreads/grandma_lottery/src/grandma_lottery.c
+++ exercises/pthreads/grandma_lottery/src/grandma_lottery.c
@@ -9,46 +9,56 @@
 #include <time.h>
 
 /**
- * @brief Genera un numero random de loteria del 1 al 100
+ * @brief Genera un numero random de loteria del 1 al 100, 
+ * pero devuelve la direccion de una variable en el stack la cual se borra de manera automatica
  * 
  * @param data Semilla del time
  * @return void* Numero random obtenido
  */
-void* loterry(void* data);
+//void* lottery_Case1(void* data);
+
+/**
+ * @brief Genera un numero random de loteria del 1 al 100, devuelve un puntero falzo
+ * 
+ * @param data Semilla del time
+ * @return void* Numero random obtenido
+ */
+void* lottery_Case2(void* data);
 
 int main(void) {  //  hilo principal
-  time_t seed = time(NULL);  //  Alamacena la llemilla
   void* number1 = NULL;
   void* number2 = NULL;
   pthread_t thread1;  //  Designa segundo hilo
   pthread_t thread2;  //  Designa tercer hilo
   //  utiliza los hilos:
-  int error1 = pthread_create(&thread1 , NULL, loterry, &seed);
-  int error2 = pthread_create(&thread2 , NULL, loterry, &seed);
+  int error1 = pthread_create(&thread1 , NULL, lottery_Case2, (void*) 1u );
+  int error2 = pthread_create(&thread2 , NULL, lottery_Case2, (void*) 2u );
   //  Validacion de hilos:
   if (error1 == EXIT_SUCCESS) {
     //  Espera hilo y obtiene su valor de retorno
     pthread_join(thread1, &number1);
-    printf("Boleto 1: %i\n", *(int*)number1);
+    printf("Boleto 1: %ld\n", (size_t)number1);
   } else {  //  En caso de error
     fprintf(stderr , "Error: could not create secundary thread\n");
   }
   if (error2 == EXIT_SUCCESS) {
     //  Espera hilo y obtiene su valor de retorno
     pthread_join(thread2, &number2);
-    printf("Boleto 2: %i\n", *(int*)number2);
+    printf("Boleto 2: %ld\n", (size_t)number2);
   } else {  //  En caso de error
     fprintf(stderr , "Error: could not create third thread\n");
   }
-  free(number1);
-  free(number2);
 }
 
-void* loterry(void* data) {
+/*void* loterry_Case1(void* data) {  // Retornan la dirección de memoria de la variable que tiene el número comprado.
+
   unsigned int* seed = (unsigned int*) data;  //  Obtiene la semilla
-  int number = rand_r(seed) % 100 + 1;  //  Genera el numero random
-  //  Genera un puntero para el numero
-  int* numberP = (int*)calloc(1, sizeof(int));
-  *numberP = number;  //  Almacena direccion del numero
-  return (void*)numberP;  //  Devuelve el numero generado
+  size_t number = rand_r(seed) % 100 + 1;  //  Genera el numero random
+  return (void*)(&number);  //  Devuelve el numero generado
+}*/
+
+void* lottery_Case2(void* data) {  // Retornan la dirección de memoria de la variable que tiene el número comprado.
+  unsigned int seed = ((size_t)data);
+  void* number = (void*)((size_t)(rand_r(&seed) % 100));  //  Genera el numero random
+  return number;  //  Devuelve el numero generado
 }

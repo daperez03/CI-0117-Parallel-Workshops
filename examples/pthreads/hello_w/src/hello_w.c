8e9101a5d2329d02c845e0ee008160eaabae3c44
Daniel Perez <daniel.perezmorera@ucr.ac.cr>
2022-08-30 02:20:24 -0600

    Avance de codigo de ejemplo hello

--- examples/pthreads/hello_w/src/hello_w.c
+++ examples/pthreads/hello_w/src/hello_w.c
@@ -16,19 +16,39 @@
  */
 void* greet(void* data);
 
-//  procedure main():
-int main(void) {
-  //  create_thread(great())
-  pthread_t thread;
-  //  CREA HILO SECUNDARIO
-  int error = pthread_create(&thread , /*attr*/ NULL , greet , /*arg*/ NULL);
-  if (error == EXIT_SUCCESS) {
-    //  print "Hello from main thread"
-    // usleep(2);  //  indeterminismo
-    printf("Hello from main thread\n");
-    pthread_join(thread, /*value_ptr*/ NULL);  //  ENVIA A DORMIR HILO PRINCIPAL
+//  procedure main(argc, argv[])
+int main(int argc, char* argv[]) {
+  #if 0  
+  Mecanismo espacial para omitir interpretacion de codigo
+  //  Ejemplo sobre los argumentos en c
+  for(int index = 0; index < argc; ++index) {
+    // imprime argumentos ingresados por el usuario
+    printf("argv[%d] = '%s'\n", index, argv[index]);
+  }
+#endif
+  int error = EXIT_SUCCESS;
+  //  thread_count := integer(argv[1])
+  long thread_count =  0; // strol(argv[1], .....)
+  //Escanea vector
+  if(sscanf(argv[1], "%ld", &thread_count) == 1) {
+    // for thread_number := 0 to thread_count do
+    for(long thread_number = 0; thread_number < thread_count; ++thread_number) {
+      //  create_thread(great(), thread_number)
+      pthread_t thread;
+      //  CREA HILO SECUNDARIO
+      error = pthread_create(&thread , /*attr*/ NULL , greet , (void*)thread_number);
+      if (error == EXIT_SUCCESS) {
+        //  print "Hello from main thread"
+        // usleep(2);  //  indeterminismo
+        printf("Hello from main thread\n");
+        pthread_join(thread, /*value_ptr*/ NULL);  //  ENVIA A DORMIR HILO PRINCIPAL
+      } else {
+        fprintf(stderr , "Error: could not create secundary thread\n");
+      }
+    }
   } else {
-    fprintf(stderr , "Error: could not create secundary thread\n");
+    fprintf(stderr , "Error: invalid thread count\n");
+    error = EXIT_FAILURE;
   }
   return error;
 }  //  end procedure
@@ -36,8 +56,8 @@ int main(void) {
 //  procedure great():
 
 void* greet(void* data) {
-  (void)data;
+  const long rank = (long)data;
   //  print "Hello from secundary thread"
-  printf("Hello from secundary thread\n");
+  printf("Hello from secundary thread %ld\n", rank);
   return NULL;
 }  //  end procedure

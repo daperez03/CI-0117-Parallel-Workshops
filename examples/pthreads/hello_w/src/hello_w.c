c8c62b2988fb70cb334f2b18cf68c848d32545af
Daniel Perez <daniel.perezmorera@ucr.ac.cr>
2022-09-01 15:37:45 -0600

    Modularizacion del codigo

--- examples/pthreads/hello_w/src/hello_w.c
+++ examples/pthreads/hello_w/src/hello_w.c
@@ -2,6 +2,8 @@
 *Copyright 2022 Daniel Perez Morera <daniel.perezmorera@ucr.ac.cr> CC-BY 4.0
 *Imprime un hello
 */
+
+
 #include <assert.h>
 #include <inttypes.h>
 #include <pthread.h>
@@ -9,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 
 /**
  * Aca se documenta las clase
@@ -18,6 +21,14 @@
  * @return void* NULL
  */
 void* greet(void* data);
+
+/**
+ * @brief Metodo el cual crea hilos de ejecucion del programa greet
+ * 
+ * @param thread_count Indica numero de hilos que se quiere ejecutar
+ * @return Numero entero indicando si la ejecucion fue exitosa o surgio un error
+ */
+int create_threads(uint64_t thread_count);
 
 //  procedure main(argc, argv[])
 int main(int argc, char* argv[]) {
@@ -38,30 +49,35 @@ int main(int argc, char* argv[]) {
   if (argc == 2) {
     if (sscanf(argv[1], "%" SCNu64, &thread_count) != 1) {
       fprintf(stderr , "Error: invalid thread count\n");
-      error = EXIT_FAILURE;
+      error = 11;
     }
   }
+  error = create_threads(thread_count);
+  return error;
+}  //  end procedure
 
-  //  for thread_number := 0 to thread_count do
+
+int create_threads(uint64_t thread_count) {
+  int error = EXIT_SUCCESS;
   pthread_t* threads = (pthread_t*)
   malloc(thread_count * sizeof(pthread_t));
 
   if (threads) {
-    for (uint64_t thread_number = 0; thread_number < thread_count;
-    ++thread_number) {
+    for (uint64_t thread_number = 0; thread_number < thread_count
+      ; ++thread_number) {
       //  create_thread(great(), thread_number)
       //  CREA HILO SECUNDARIO
       error = pthread_create(&threads[thread_number] ,
       /*attr*/ NULL , greet , (void*)thread_number);
-      if (error == EXIT_SUCCESS) {
-      } else {
+      if (error != EXIT_SUCCESS) {
         fprintf(stderr , "Error: could not create secundary thread\n");
+        error = 21;
       }
     }
 
     printf("Hello from main thread\n");
-    for (uint64_t thread_number = 0; thread_number < thread_count;
-    ++thread_number) {
+    for (uint64_t thread_number = 0; thread_number < thread_count
+      ; ++thread_number) {
       //  usleep(2);  //  indeterminismo
       //  ENVIA A DORMIR HILO PRINCIPAL
       pthread_join(threads[thread_number], /*value_ptr*/ NULL);
@@ -70,17 +86,15 @@ int main(int argc, char* argv[]) {
       //  print "Hello from main thread"
     free(threads);
   } else {
-
     fprintf(stderr, "Error: could not allocate %" PRIu64
     "threads\n", thread_count);
-    error = EXIT_FAILURE;
-
+    error = 22;
   }
   return error;
-}  //  end procedure
+}
+
 
   //  procedure great():
-
 void* greet(void* data) {
   const uint64_t rank = (uint64_t)data;
   //  print "Hello from secundary thread"

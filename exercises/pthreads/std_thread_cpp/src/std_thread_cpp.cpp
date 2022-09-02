8c58f8148ef91951c038d7b0b5747f8e2d23221d
Daniel Alberto Perez Morera <c15906@ECCI104L408200.ecci.ucr.ac.cr>
2022-09-02 12:01:16 -0600

    Implementacion de hilos en c++

--- exercises/pthreads/std_thread_cpp/src/std_thread_cpp.cpp
+++ exercises/pthreads/std_thread_cpp/src/std_thread_cpp.cpp
@@ -19,29 +19,14 @@ typedef struct shared_data {
   //  thread_private_data_t
 typedef struct private_data {
   uint64_t thread_number;
-  share_data_t* shared_data;
+  std::shared_ptr<share_data_t> shared_data;
 } private_data_t;
 
-/**
- * Aca se documenta las clase
- * @brief  Genera un mensaje en la salida standar
- * 
- * @param data NULL
- * @return void* NULL
- */
-void* greet(void* data);
-
-/**
- * @brief Metodo el cual crea hilos de ejecucion del programa greet
- * 
- * @param thread_count Indica numero de hilos que se quiere ejecutar
- * @return Numero entero indicando si la ejecucion fue exitosa o surgio un error
- */
-int create_threads(share_data_t* share_data);
+int create_threads(std::shared_ptr<share_data_t> share_data);
+int greet(std::shared_ptr<private_data_t> private_data);
 
 //  procedure main(argc, argv[])
 int main(int argc, char* argv[]) {
-  auto start = std::chrono::high_resolution_clock::now();
   int error = EXIT_SUCCESS;
   //  thread_count := integer(argv[1])
   uint64_t thread_count =  std::thread::hardware_concurrency();
@@ -60,35 +45,34 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  share_data_t* share_data = new share_data_t[1];
+  std::shared_ptr<share_data_t> share_data(new share_data_t);
   if (share_data) {
     share_data->thread_count = thread_count;
+    auto start = std::chrono::high_resolution_clock::now();
     error = create_threads(share_data);
-    delete []share_data;
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    std::cout << "Time of the process: "<< diff.count() << " s\n";
   } else {
     std::cerr << "Error: could not allocate shared data\n";
     error = 12;
   }
-  auto end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> diff = end - start;
-  std::cout << "Time of the process: "<< diff.count() << " s\n";
   return error;
 }  //  end procedure
 
 
-int create_threads(share_data_t* share_data) {
+int create_threads(std::shared_ptr<share_data_t> share_data) {
   int error = EXIT_SUCCESS;
-  pthread_t* threads = new pthread_t[share_data->thread_count];
-  private_data_t* private_data = new private_data_t[share_data->thread_count];
-  if (threads && private_data) {
+  std::vector<std::thread> threads;
+  std::vector<private_data_t> private_data(share_data->thread_count);
+  if (1) {
     for (uint64_t thread_number = 0; thread_number < share_data->thread_count
       ; ++thread_number) {
         private_data[thread_number].thread_number = thread_number;
         private_data[thread_number].shared_data = share_data;
       //  create_thread(great(), thread_number)
       //  CREA HILO SECUNDARIO
-      error = pthread_create(&threads[thread_number] ,
-      /*attr*/ NULL , greet , &private_data[thread_number]);
+      threads.emplace_back(std::thread(greet, private_data[thread_number]));
       if (error != EXIT_SUCCESS) {
         std::cerr << "Error: could not create secundary thread\n";
         error = 21;
@@ -100,12 +84,9 @@ int create_threads(share_data_t* share_data) {
       ; ++thread_number) {
       //  usleep(2);  //  indeterminismo
       //  ENVIA A DORMIR HILO PRINCIPAL
-      pthread_join(threads[thread_number], /*value_ptr*/ NULL);
+      threads[thread_number].join();
     }
-
       //  print "Hello from main thread"
-    delete []private_data;
-    delete []threads;
   } else {
     std::cerr << "Error: could not allocate " << share_data->thread_count <<
     "threads\n";
@@ -116,10 +97,9 @@ int create_threads(share_data_t* share_data) {
 
 
   //  procedure great():
-void* greet(void* data) {
-  private_data_t* private_data = (private_data_t*)data;
-  share_data_t* shared_data = private_data->shared_data;
+int greet(std::shared_ptr<private_data_t> private_data) {
+  std::shared_ptr<share_data_t> shared_data = private_data->shared_data;
   //  print "Hello from secundary thread"
-  std::cout << "Hello from secundary thread " <<private_data->thread_number <<" of " << shared_data->thread_count <<"\n";
-  return NULL;
+  std::cout << "Hello from secundary thread " << private_data->thread_number <<" of " << shared_data->thread_count <<"\n";
+  return EXIT_SUCCESS;
 }  //  end procedure

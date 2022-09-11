a0225e0fafe3add85dc605eabe9915ef8b612574
Daniel Perez <daniel.perezmorera@ucr.ac.cr>
2022-09-11 08:19:12 -0600

    Se implementaron las funciones responsables de la lectura y verificacion de datos

--- homeworks/goldbach_serial/src/buffer.c
+++ homeworks/goldbach_serial/src/buffer.c
@@ -1,44 +1,50 @@
 #include "buffer.h"
 
+
+uint64_t save_data(number_t*, int64_t, uint64_t);
+
+
+
 uint64_t readData(goldbach_sums_t* my_goldbach_sums){
   uint64_t error = EXIT_SUCCESS;
   int64_t number_read = 0;
-  uint64_t scanner_status = 1;
-  while (scanner_status == 1) {  //  falta validar numero
-    errno = 0;
-    scanner_status = fscanf(stdin, "%" SCNi64, &number_read);
-    if(scanner_status == 1 && !errno){
-      if(my_goldbach_sums->count == my_goldbach_sums->capacity){
-        error = resize(my_goldbach_sums);
+  int64_t scanner_status = 1;
+  errno = 0;
+  while (scanner_status != EOF) {
+    scanner_status = scanf("%" SCNd64, &number_read);
+    if (!errno && scanner_status == 1) {
+      if(my_goldbach_sums->count == my_goldbach_sums->capacity - 1){
+        error = resize_numbers(my_goldbach_sums);
       }
       if (error == EXIT_SUCCESS) {
-        if(number_read >= 0){
-          error = save_data(&my_goldbach_sums->numbers[my_goldbach_sums->count],
-            number_read, positive_t, 1);
-        } else {
-          error = save_data(&my_goldbach_sums->numbers[my_goldbach_sums->count],
-            number_read, negative_t, 24);
-        } 
+        if(number_read > 3){
+          error = save_data(&my_goldbach_sums->numbers[my_goldbach_sums->count++],
+            number_read, 1);
+        } else if(number_read < -3) {
+          error = save_data(&my_goldbach_sums->numbers[my_goldbach_sums->count++],
+            number_read, 30);
+        } else {          
+          error = save_data(&my_goldbach_sums->numbers[my_goldbach_sums->count++],
+            number_read, 0);
+        }
+        
       }
-    } else if(errno || scanner_status != 1) {
+    } else if(scanner_status == 0 || errno) {
       fprintf(stderr,"Error: invalid input\n");
       error = 21;
-      scanner_status = 0;
+      break;
     }
   }
   return error;
 }
 
-uint64_t save_data(number_t* number, uint16_t number_read, enum resolt_t resolt, uint16_t capacity) {
+uint64_t save_data(number_t* struct_number, int64_t number_read, uint64_t capacity) {
   uint64_t error = EXIT_SUCCESS;
-  number->number = number_read;
-  number->result = resolt;
-  number->capacity = capacity;
-  number->sum_count = 0;
-  number->sums = (uint64_t*)calloc(capacity, sizeof(uint64_t));
-  if (number->sums == NULL) {
-    error = 22;
-    fprintf(stderr, "Error: could not create array of sums\n");
+  struct_number->number = number_read;
+  struct_number->capacity = capacity;
+  struct_number->sum_count = 0;
+  if(capacity){
+    struct_number->sums = (uint64_t*)calloc(capacity, sizeof(uint64_t));
   }
   return error;
 }

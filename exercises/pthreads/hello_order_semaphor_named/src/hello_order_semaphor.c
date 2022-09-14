6292fd0c1dbc56a8a4c1fc2931023d96d23dfa65
Daniel Perez <daniel.perezmorera@ucr.ac.cr>
2022-09-14 17:42:22 -0600

    Solucion de error en semaforo

--- exercises/pthreads/hello_order_semaphor_named/src/hello_order_semaphor.c
+++ exercises/pthreads/hello_order_semaphor_named/src/hello_order_semaphor.c
@@ -3,7 +3,6 @@
 *Imprime un hello
 */
 
-
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -46,6 +45,7 @@ void* greet(void* data);
  */
 int create_threads(shared_data_t* share_data);
 
+
 //  procedure main(argc, argv[])
 int main(int argc, char* argv[]) {
   int error = EXIT_SUCCESS;
@@ -61,10 +61,22 @@ int main(int argc, char* argv[]) {
   }
 
   shared_data_t* share_data = (shared_data_t*)
-    calloc(1, sizeof(shared_data_t));  
+    calloc(1, sizeof(shared_data_t));
   if (share_data) {
     share_data->can_greet = (sem_t**)calloc(thread_count, sizeof(sem_t*));
     share_data->thread_count = thread_count;
+    share_data->_names = (char**)calloc(thread_count, sizeof(char*));
+
+    for (uint64_t thread_number = 0; thread_number < thread_count
+      ; ++thread_number) {
+      share_data->_names[thread_number] = (char*)calloc(256, sizeof(char));
+      snprintf(share_data->_names[thread_number]
+        , (size_t)256, "sem%zu", thread_number);
+      share_data->can_greet[thread_number] =
+        sem_open(share_data->_names[thread_number]
+          , O_CREAT | O_EXCL, 0644, !thread_number);
+    }
+
     if (share_data->can_greet) {
       struct timespec start_time, finish_time;
       clock_gettime(CLOCK_MONOTONIC, &start_time);
@@ -73,11 +85,12 @@ int main(int argc, char* argv[]) {
       double elapsed_time = finish_time.tv_sec - start_time.tv_sec +
         (finish_time.tv_nsec - start_time.tv_nsec) * 1e-9;  //  1e-9 = 1 ^-9
       printf("Execution time: %9lfs\n", elapsed_time);
-      for (uint64_t thread_number = 0; thread_number < share_data->thread_count; ++thread_number) {
-        char _name[256];
-        snprintf(_name, (size_t)256, (const char*)"sem%zu", thread_number);
-        sem_unlink((const char*)_name);
+      for (uint64_t thread_number = 0; thread_number
+      < share_data->thread_count; ++thread_number) {
+        sem_unlink(share_data->_names[thread_number]);
+        free(share_data->_names[thread_number]);
       }
+      free(share_data->_names);
       free(share_data->can_greet);
     } else {
       fprintf(stderr , "Error: could not allocate semaphoro\n");
@@ -102,45 +115,38 @@ int create_threads(shared_data_t* share_data) {
     for (uint64_t thread_number = 0; thread_number < share_data->thread_count
       ; ++thread_number) {
       //  can_greet[thread_number] := create_semaphore(not thread_number)
-      errno = EXIT_SUCCESS;
-      char _name[256];
-      snprintf(_name, (size_t)256, (const char*)"sem%zu", thread_number);
-      share_data->can_greet[thread_number] =
-        sem_open((const char*) _name, O_CREAT | O_EXCL, 0644, !thread_number);
-      if (error == EXIT_SUCCESS && errno == EXIT_SUCCESS) {
+      if (share_data->can_greet[thread_number] != SEM_FAILED) {
         private_data[thread_number].thread_number = thread_number;
         private_data[thread_number].shared_data = share_data;
         //  create_thread(great(), thread_number)
         //  CREA HILO SECUNDARIO
         error = pthread_create(&threads[thread_number] ,
-        /*attr*/ NULL , greet , &private_data[thread_number]);
+          /*attr*/ NULL , greet , &private_data[thread_number]);
         if (errno != EXIT_SUCCESS && error == EXIT_SUCCESS) {
           fprintf(stderr , "Error: could not create secundary thread\n");
           return 21;
         }
-
-        sem_close(share_data->can_greet[thread_number]);
+        //  sem_close(share_data->can_greet[thread_number]);
       } else {
         fprintf(stderr , "Error: could not init semaphore\n");
-        return 22;
+        error = 22;
+        break;
       }
     }
 
     printf("Hello from main thread\n");
-    for (uint64_t thread_number = 0; thread_number < share_data->thread_count
-      ; ++thread_number) {
-      //  usleep(2);  //  indeterminismo
-      //  ENVIA A DORMIR HILO PRINCIPAL
-      pthread_join(threads[thread_number], /*value_ptr*/ NULL);
+    for (uint64_t thread_number = 0; thread_number
+      < share_data->thread_count; ++thread_number) {
+      if (error == (int)EXIT_SUCCESS) {
+        pthread_join(threads[thread_number], /*value_ptr*/ NULL);
+      }
     }
-
-      //  print "Hello from main thread"
     free(private_data);
     free(threads);
   } else {
     fprintf(stderr, "Error: could not allocate %" PRIu64
     "threads\n", share_data->thread_count);
-    error = 22;
+    error = 23;
   }
   return error;
 }
@@ -152,15 +158,22 @@ void* greet(void* data) {
   private_data_t* private_data = (private_data_t*)data;
   shared_data_t* shared_data = private_data->shared_data;
   //  wait(can_greet[thread_number])
-  sem_wait(shared_data->can_greet[private_data->thread_number]);
+  printf("%" SCNu64 "\n", private_data->thread_number);
+  int error = sem_wait(shared_data->can_greet[private_data->thread_number]);
+  if (error) {
+    fprintf(stderr, "error: could not wait for semaphore %d\n", errno);
+  }
 
   //  print "Hello from secundary thread"
   printf("Hello from secundary thread %" PRIu64 " of %" PRIu64 "\n"
   , private_data->thread_number , shared_data->thread_count);
   //  signal(can_greet[(thread_number + 1) mod thread_count])
-  const uint64_t next_thread = (private_data->thread_number + 1) 
+  const uint64_t next_thread = (private_data->thread_number + 1)
     % shared_data->thread_count;
-  sem_post(shared_data->can_greet[next_thread]);
+  error = sem_post(shared_data->can_greet[next_thread]);
+  if (error) {
+    fprintf(stderr, "error: could not increment semaphore%d \n", errno);
+  }
 
   return NULL;
 }  //  end procedure

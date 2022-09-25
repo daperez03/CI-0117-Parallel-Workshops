eda0098ddb5453ab05c52248644951c153a95519
Daniel Perez <daniel.perezmorera@ucr.ac.cr>
2022-09-24 23:09:54 -0600

    Implementacion de codigo

---
+++ exercises/pthreads/network_simul_packet_loss2/src/simulation/ProducerConsumerTest.hpp
@@ -0,0 +1,65 @@
+/// @copyright 2020 ECCI, Universidad de Costa Rica. All rights reserved
+/// This code is released under the GNU Public License version 3
+/// @author Jeisson Hidalgo-Céspedes <jeisson.hidalgo@ucr.ac.cr>
+
+#ifndef PRODUCERCONSUMERTEST_HPP
+#define PRODUCERCONSUMERTEST_HPP
+
+#include <cstddef>
+#include <vector>
+
+#include "common.hpp"
+#include "./prodcons/Consumer.hpp"
+#include "NetworkMessage.hpp"
+
+// Forward declarations
+class ConsumerTest;
+class AssemblerTest;
+class DispatcherTest;
+class ProducerTest;
+
+/**
+ * @brief Controller class that simulates the message passing between
+ * producer and cosumers through a dispatcher
+ */
+class ProducerConsumerTest {
+  DISABLE_COPY(ProducerConsumerTest);
+
+ private:
+  /// Number of packages to be produced
+  size_t packageCount = 0;
+  /// Number of consumer threads
+  size_t consumerCount = 0;
+  /// Delay of producer to create a package, negative for max random
+  int productorDelay = 0;
+  /// Delay of dispatcher to dispatch a package, negative for max random
+  int dispatcherDelay = 0;
+  /// Delay of consumer to consume a package, negative for max random
+  int consumerDelay = 0;
+  /// Probality of losing packages
+  float losingPackages = 0;
+
+ private:
+  /// Producer of the simulated network messages
+  ProducerTest* producer = nullptr;
+  /// A dispatcher of the of the simulated network messages
+  DispatcherTest* dispatcher = nullptr;
+  /// A assembler of the simulated
+  AssemblerTest* assembler;
+  /// Consumers of the simulated network messages
+  std::vector<Consumer<NetworkMessage>*> consumers;
+
+ public:
+  /// Constructor
+  ProducerConsumerTest() = default;
+  /// Destructor
+  ~ProducerConsumerTest();
+  /// Start the simulation
+  int start(int argc, char* argv[]);
+
+ private:
+  /// Analyze the command line arguments
+  int analyzeArguments(int argc, char* argv[]);
+};
+
+#endif  // PRODUCERCONSUMERTEST_HPP

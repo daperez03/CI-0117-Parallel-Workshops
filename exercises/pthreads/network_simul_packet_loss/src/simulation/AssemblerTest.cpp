9012364b985a945408059c819fc975947cd50444
Daniel Perez <daniel.perezmorera@ucr.ac.cr>
2022-09-24 20:36:19 -0600

    Modificacion AssemblerTest, ahora hereda de clase abstracta Assember

--- exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.cpp
+++ exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.cpp
@@ -10,25 +10,16 @@ AssemblerTest::AssemblerTest(float losingPackages, size_t consumerCount) {
 }
 
 int AssemblerTest::run() {
-  assert(this->consumingQueue);
-  while (true) {
-      // Get the next data to consume, or block while queue is empty
-    const NetworkMessage& data = this->consumingQueue->pop();
-      // If data is the stop condition, stop the loop
-    if ( data == this->stopCondition ) {
-      break;
-    }
-    const float probabilityOfLose =
-        1 + (Util::random(0, 9999999) / 100000.0);
-    if (probabilityOfLose > this->losingPackages) {
-      int target = 1 + Util::random(0
-        , static_cast<int>(this->consumerCount));
-      this->producingQueue->push
-        (NetworkMessage(target, data.source, data.messageNumber));
-    }  //  else {
-        //  std::cout << probabilityOfLose << "-"
-          //  <<this->losingPackages<< std::endl;
-      //  }
+  consumeForever();
+  return EXIT_SUCCESS;
+}
+
+void AssemblerTest::consume(NetworkMessage data) {
+  const float probabilityOfLose = (Util::random(0, 100000000) / 1000000.0);
+  if (probabilityOfLose > this->losingPackages) {
+    int target = 1 + Util::random(0
+      , static_cast<int>(this->consumerCount));
+    this->producingQueue->push
+      (NetworkMessage(target, data.source, data.messageNumber));
   }
-  return 0;
 }
--- exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.hpp
+++ exercises/pthreads/network_simul_packet_loss/src/simulation/AssemblerTest.hpp
@@ -3,17 +3,18 @@
 #ifndef ASSEMBLERTEST_HPP
 #define ASSEMBLERTEST_HPP
 
-#include "ConsumerTest.hpp"
+#include "./prodcons/Assembler.hpp"
 #include "ProducerTest.hpp"
 
 class AssemblerTest
-  : public ConsumerTest
-    , public ProducerTest {
+  : public Assembler<NetworkMessage, NetworkMessage> {
  private:
   float losingPackages;
+  size_t consumerCount;
  public:
   AssemblerTest(float losingPackages, size_t consumerCount);
   int run() override;
+  void consume(NetworkMessage data) override;
 };
 
 #endif

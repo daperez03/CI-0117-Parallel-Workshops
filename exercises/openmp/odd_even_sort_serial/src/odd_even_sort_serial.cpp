9391f04bc5fe2f3b31e048c4026bc76e2ccfc8a0
Daniel Perez <daniel.perezmorera@ucr.ac.cr>
2022-10-29 11:10:57 -0600

    Implementacio de algoritmo de ordenamiento sort de manera serial

---
+++ exercises/openmp/odd_even_sort_serial/src/odd_even_sort_serial.cpp
@@ -0,0 +1,44 @@
+#include <cstdlib>
+#include <iostream>
+
+void concurrent_odd_even_sort(size_t n, double* arr);
+void swap(double& number1, double& number2);
+
+int main(int argc, char* argv[]) {
+  size_t n = 0;
+  if (argc == 2) {
+    n = atoi(argv[1]);
+  }
+  double* arr = new double[n];
+  for (size_t i = 0; i < n; i++)
+    arr[i] = (double)(rand() % 100000)
+      + ((double)(rand() % 100000)/1000000);
+  concurrent_odd_even_sort(n, arr);
+  for (size_t i = 0; i < n; i++) printf("%f - ", arr[i]);
+  std::cout << std::endl;
+  delete []arr;
+}
+
+void concurrent_odd_even_sort(size_t n, double* arr) {
+  for (size_t phase = 0; phase < n; ++phase) {
+    if (phase % 2 == 0) {
+      for (size_t i = 1; i < n; i += 2) {
+          if (arr[i - 1] > arr[i]) {
+            swap(arr[i - 1], arr[i]);
+          }
+      }
+    } else {
+      for (size_t i = 1; i < n - 1; i += 2) {
+        if (arr[i] > arr[i + 1]) {
+          swap(arr[i], arr[i + 1]);
+        }
+      }
+    }
+  }
+}
+
+void swap(double& number1, double& number2) {
+  double copy_number = number1;
+  number1 = number2;
+  number2 = copy_number;
+}
